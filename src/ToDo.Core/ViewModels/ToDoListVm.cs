c55aa2f86f844b5d486e6fb8ad35688d0c26ca9a
vvavilov <v.vavilov2@gmail.com>
2020-01-06 17:37:10 +0300

    Add NSwagger

---
+++ src/ToDo.Core/ViewModels/ToDoListVm.cs
@@ -0,0 +1,7 @@
+namespace ToDo.Core.ViewModels
+{
+    public class ToDoListVm
+    {
+        
+    }
+}
---
+++ src/ToDo.Domain/Entities/ToDoList.cs
@@ -0,0 +1,7 @@
+namespace ToDo.Domain.Entities
+{
+    public class ToDoList
+    {
+        
+    }
+}
